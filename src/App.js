9f78300fd6c6cde1e243632f29eb6e42dbed6810
Mik <strangedk@yandex.ru>
2018-06-16 07:56:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    this.state = { message: "" }
+  }
+  getNextImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(({ message }) => {
+      this.setState({ message });
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={this.state.message} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to livecoding</h1>
         </header>
         <p className="App-intro">
